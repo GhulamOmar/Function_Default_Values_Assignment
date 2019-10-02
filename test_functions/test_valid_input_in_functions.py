d864a6cc0f6bddf8ec57925f79950520879867a1
Omar <gomar@dmacc.edu>
2019-10-02 15:37:21 -0700

    first test passed

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -4,16 +4,9 @@ from more_functions import validate_input_in_functions
 
 class MyTestCase(unittest.TestCase):
     def test_score_valid(self):
-        self.assertTrue(validate_input_in_functions.score_input("python", 3))
+        self.assertTrue(validate_input_in_functions.score_input("python", 4))
 
-    def test_above_range_input(self):
-        self.assertFalse(validate_input_in_functions.score_input("Invalid value, above the range"), 101)
 
-    def test_score_below_range_input(self):
-        self.assertFalse(validate_input_in_functions.score_input("Invalid value, below the range"), -44)
-
-    def test_test_score_non_numeric(self):
-        self.assertRaises(ValueError, validate_input_in_functions.score_input("Non numeric value"), )
 
 
 if __name__ == '__main__':
