5873346d163f1fe10ea23b13ffc85d83238c3c26
Omar <gomar@dmacc.edu>
2019-10-02 15:33:39 -0700

    test four

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -12,6 +12,9 @@ class MyTestCase(unittest.TestCase):
     def test_score_below_range_input(self):
         self.assertFalse(validate_input_in_functions.score_input("Invalid value, below the range"), -44)
 
+    def test_test_score_non_numeric(self):
+        self.assertRaises(ValueError, validate_input_in_functions.score_input("Non numeric value"), )
+
 
 if __name__ == '__main__':
     unittest.main()
